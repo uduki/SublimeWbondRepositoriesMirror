df88b0929b835640f8915b9c31df7f810ea7c0c6
uduki <cat@oborogin.org>
2013-05-24 12:43:59 +0900

    initial commit

---
+++ wbond-repos-mirror.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+REPOS_JSON=https://sublime.wbond.net/repositories.json
+TMP_DIR=/tmp/swrmirror
+MSG_FILE=msg
+MSG_PATH=$TMP_DIR/$MSG_FILE
+DEST_FILE=$1
+DEST_TMP_FILE=$TMP_DIR/`basename $DEST_FILE`
+LOG_FILE=$2
+
+wget -o $MSG_PATH -S -O $DEST_TMP_FILE $REPOS_JSON
+RET_CODE=`grep "HTTP/1.1 200 OK" $MSG_PATH`
+
+if [ "$RET_CODE" != "" ]; then
+	cp $DEST_TMP_FILE $DEST_FILE
+	EXIT_CODE=0
+else
+	EXIT_CODE=1
+fi
+
+cat $MSG_PATH >> $LOG_FILE
+
+exit $EXIT_CODE
